ea7af38866f8e3e08bbd0e5d5272c54007fc5b14
Christina Kidwell <43004589+kidwellc@users.noreply.github.com>
2018-10-06 01:06:13 -0400

    Installed CLion

---
+++ cmake-build-debug/CMakeFiles/gv343_game_section2.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/ftp/Ftp.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/island/Island.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/joystick/Joystick.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/opengl/OpenGL.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/pong/Pong.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/shader/Shader.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/sockets/Sockets.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/sockets/TCP.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/sockets/UDP.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/sound/Sound.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/sound-capture/SoundCapture.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/voip/Client.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/voip/Server.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/voip/VoIP.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/window/Window.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/SFML-2.5.0/share/SFML/examples/X11Example/X11.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/Collision.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/Game.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/main.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/Monster.cpp.obj"
+  "CMakeFiles/gv343_game_section2.dir/Person.cpp.obj"
+  "gv343_game_section2.pdb"
+  "gv343_game_section2.exe"
+  "gv343_game_section2.exe.manifest"
+  "libgv343_game_section2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gv343_game_section2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/gv343_game_section2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gv343_game_section2.
+# This may be replaced when dependencies are built.
